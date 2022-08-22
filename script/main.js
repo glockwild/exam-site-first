4dc4d19bb8846235cba8c83900d7c30ba33acbce
denys <glockwilder@gmail.com>
2022-08-22 17:18:40 +0200

    Fixed menu

--- script/main.js
+++ script/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    let options = {};
+    let options = {
+       
+    };
     var instances = M.Sidenav.init(elems, options);
   });
